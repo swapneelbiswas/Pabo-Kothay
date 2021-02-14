ad3833dcd72e90d952bf80cd4b3fd5d3c1e8724b
Nafi62742 <61117249+Nafi62742@users.noreply.github.com>
2021-02-14 21:42:47 +0600

    startup

--- app/src/main/java/com/example/pabokothay/LogInPage.java
+++ app/src/main/java/com/example/pabokothay/LogInPage.java
@@ -2,7 +2,9 @@ package com.example.pabokothay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogInPage extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LogInPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in_page);
+        getSupportActionBar().hide();
+    }
+
+    public void goMainActivity(View view){
+        Intent intent= new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/pabokothay/MainActivity.java
+++ app/src/main/java/com/example/pabokothay/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.pabokothay;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText eText;
+    Button btn,btn1;
+    private DrawerLayout Dl;
+    private ActionBarDrawerToggle dToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in_page);
+        setContentView(R.layout.activity_main);
+        Dl=findViewById(R.id.drawer_layout);
+        dToggle = new ActionBarDrawerToggle(this,Dl,R.string.Open,R.string.Close);
+        Dl.addDrawerListener(dToggle);
+        dToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(dToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
