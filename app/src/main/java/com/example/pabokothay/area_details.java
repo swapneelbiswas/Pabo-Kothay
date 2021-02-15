5f55002bc44dbea573fa43f28944a9ecaaaff360
Nafi62742 <61117249+Nafi62742@users.noreply.github.com>
2021-02-15 15:00:43 +0600

    IDK

--- app/src/main/java/com/example/pabokothay/area_details.java
+++ app/src/main/java/com/example/pabokothay/area_details.java
@@ -2,11 +2,19 @@ package com.example.pabokothay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-import com.google.android.gms.maps.GoogleMapOptions;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class area_details extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,14 @@ public class area_details extends AppCompatActivity {
         setContentView(R.layout.activity_area_details);
         getSupportActionBar().hide();
 
+    }
+    public void gotolink(View view){
+        gotoUrl("https://www.google.com/");
+
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri= Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
     }
 }
