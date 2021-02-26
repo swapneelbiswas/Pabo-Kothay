022e3a675d86f3cd4641cb08209d00dbad1e0157
LutfunnesaSuzana <lutfunnesasuzana@gmail.com>
2021-02-27 01:40:56 +0600

    bookSearch added

--- app/src/main/java/com/example/pabokothay/MainActivity.java
+++ app/src/main/java/com/example/pabokothay/MainActivity.java
@@ -11,21 +11,78 @@ import android.provider.ContactsContract;
 import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText eText;
     Button btn,btn1;
+    SearchView vSearchViewMain;
+    ListView vListViewMain;
     private DrawerLayout Dl;
     private ActionBarDrawerToggle dToggle;
+
+    ArrayList<String> mainList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Dl=findViewById(R.id.drawer_layout);
+        dToggle = new ActionBarDrawerToggle(this,Dl,R.string.Open,R.string.Close);
+        Dl.addDrawerListener(dToggle);
+        dToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        vSearchViewMain= (SearchView)findViewById(R.id.search_bar);
+        vListViewMain=(ListView)findViewById(R.id.mainList);
+
+        mainList=new ArrayList<String>();
+        mainList.add(" book ");
+        mainList.add("sport");
+        mainList.add("furmitre");
+        mainList.add("food");
+
+
+
+        System.out.println(mainList.get(0));
+        adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mainList);
+
+        vListViewMain.setAdapter(adapter);
+
+        vSearchViewMain.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        vListViewMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Intent intent= new Intent(view.getContext(),book_search.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
 //        Dl=findViewById(R.id.drawer_layout);
 //        dToggle = new ActionBarDrawerToggle(this,Dl,R.string.Open,R.string.Close);
 //        Dl.addDrawerListener(dToggle);
@@ -38,10 +95,7 @@ public class MainActivity extends AppCompatActivity {
 //            return true;
 //        return super.onOptionsItemSelected(item);
 //    }
-    public void goArea(View view){
-        Intent intent= new Intent(this,area_details.class);
-        startActivity(intent);
-    }
+
     public void goProfile(View view){
         Intent intent= new Intent(this, profile.class);
         startActivity(intent);
---
+++ app/src/main/java/com/example/pabokothay/book_search.java
@@ -0,0 +1,72 @@
+package com.example.pabokothay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+
+public class book_search extends AppCompatActivity {
+
+    SearchView vSearchView;
+    ListView vListView;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_search);
+
+        vSearchView= (SearchView)findViewById(R.id.searchViewBook);
+        vListView=(ListView)findViewById(R.id.bookStoreList);
+
+        list=new ArrayList<String>();
+        list.add("momotaj book store");
+        list.add("Rjsahi book store");
+        list.add("Naraynganj  book store");
+        list.add("modumoti book store");
+        list.add("Rk book store");
+        list.add("BK dash book store");
+        list.add("sugondha book store");
+
+
+        System.out.println(list.get(0));
+        adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
+
+        vListView.setAdapter(adapter);
+
+        vSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
+        vListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Intent intent= new Intent(view.getContext(),area_details.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }
+
+}
