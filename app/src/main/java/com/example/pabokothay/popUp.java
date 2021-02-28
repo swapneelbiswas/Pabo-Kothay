2d4d4d93ce01fa14e38062862d4b5f8e6c2332ab
Swapneel Biswas <swapneelbiswas99@gmail.com>
2021-02-28 22:42:45 +0600

    btn bg added

---
+++ app/src/main/java/com/example/pabokothay/popUp.java
@@ -0,0 +1,38 @@
+package com.example.pabokothay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class popUp extends AppCompatActivity{
+    Button bt;
+    Button bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_up);
+
+        bt = (Button)findViewById(R.id.btngonewC);
+        bt.setOnClickListener(this::goNewAccount);
+
+        bt2 = (Button)findViewById(R.id.btngonewS);
+        bt2.setOnClickListener(this::goNewShopAccount);
+    }
+
+
+
+
+    public void goNewAccount(View view){
+        Intent intent= new Intent(this,newAccount.class);
+        startActivity(intent);
+    }
+    public void goNewShopAccount(View view){
+        Intent intent= new Intent(this,new_shop_account.class);
+        startActivity(intent);
+    }
+
+
+}
