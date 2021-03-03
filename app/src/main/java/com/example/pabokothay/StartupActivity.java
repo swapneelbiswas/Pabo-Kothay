a55a26ccfca9d5bde504b0737a74ea12d18b1a2f
Swapneel Biswas <swapneelbiswas99@gmail.com>
2021-03-03 23:13:41 +0600

    new startup

--- app/src/main/java/com/example/pabokothay/StartupActivity.java
+++ app/src/main/java/com/example/pabokothay/StartupActivity.java
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class StartupActivity extends AppCompatActivity {
 
-    private static int splashTime =3000;
+    private static int splashTime =10000;
     Animation topanime,animbot,fadein;
     ImageView t1,t2;
 
@@ -22,12 +23,12 @@ public class StartupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_startup);
         //getSupportActionBar().hide();
 
-        fadein= AnimationUtils.loadAnimation(this,R.anim.fade_in);
-        t1=findViewById(R.id.toptext);
-        t1.setAnimation(fadein);
-
+//        fadein= AnimationUtils.loadAnimation(this,R.anim.fade_in);
+//        t1=findViewById(R.id.toptext);
+//        t1.setAnimation(fadein);
+//
         animbot= AnimationUtils.loadAnimation(this,R.anim.botomanim);
-        t2=findViewById(R.id.bottomtext);
+        t2=findViewById(R.id.srt_imageView);
         t2.setAnimation(animbot);
 
 
@@ -39,5 +40,11 @@ public class StartupActivity extends AppCompatActivity {
                 finish();
             }
         }, splashTime);
+
+
+    }
+    public void goLogInPage(View view){
+        Intent intent= new Intent(this,LogInPage.class);
+        startActivity(intent);
     }
 }
