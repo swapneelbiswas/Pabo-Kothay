ca63bbceeb6ec73ecc0608658745055c8c2aba16
Swapneel Biswas <swapneelbiswas99@gmail.com>
2021-03-04 22:38:13 +0600

    design changed

--- app/src/main/java/com/example/pabokothay/StartupActivity.java
+++ app/src/main/java/com/example/pabokothay/StartupActivity.java
@@ -1,6 +1,7 @@
 package com.example.pabokothay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +10,15 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class StartupActivity extends AppCompatActivity {
 
-    private static int splashTime =10000;
+    private static int splashTime =4000;
     Animation topanime,animbot,fadein;
-    ImageView t1,t2;
+    CardView t2;
+    TextView tv;
 
 
     @Override
@@ -23,13 +27,16 @@ public class StartupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_startup);
         //getSupportActionBar().hide();
 
-//        fadein= AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        fadein= AnimationUtils.loadAnimation(this,R.anim.fade_in);
 //        t1=findViewById(R.id.toptext);
 //        t1.setAnimation(fadein);
 //
         animbot= AnimationUtils.loadAnimation(this,R.anim.botomanim);
-        t2=findViewById(R.id.srt_imageView);
+        t2=findViewById(R.id.srt_card);
         t2.setAnimation(animbot);
+        tv= findViewById(R.id.srt_tv);
+        tv.setAnimation(animbot);
+
 
 
         new Handler().postDelayed(new Runnable() {
