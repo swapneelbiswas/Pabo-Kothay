0a2949a86d57c385befb6cad42f671d13ce08602
Nafi62742 <61117249+Nafi62742@users.noreply.github.com>
2021-03-22 21:45:42 +0600

    Remember Me without remembering

--- app/src/main/java/com/example/pabokothay/StartupActivity.java
+++ app/src/main/java/com/example/pabokothay/StartupActivity.java
@@ -1,5 +1,6 @@
 package com.example.pabokothay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
@@ -7,16 +8,19 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -25,6 +29,9 @@ public class StartupActivity extends AppCompatActivity {
     CardView t2;
     TextView tv;
     FirebaseAuth mFirebaseAuth;
+    private FirebaseUser fUser;
+    private DatabaseReference databaseReference;
+    String userID,parentDb="Users",UserType="Customers",UserType2="ShopKeeper";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,21 +56,49 @@ public class StartupActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-//                FirebaseUser mFirebaseUser =mFirebaseAuth.getCurrentUser();
-//                if(mFirebaseUser!=null){
-//                    Intent intent = new Intent(StartupActivity.this, MainActivity.class);
-//                    startActivity(intent);
-//                    Animatoo.animateSlideLeft(StartupActivity.this);
-//                    finish();
-//                }else {
+               final DatabaseReference RootRef;
+                RootRef = FirebaseDatabase.getInstance().getReference();
+                fUser = FirebaseAuth.getInstance().getCurrentUser();
+                databaseReference= FirebaseDatabase.getInstance().getReference("Users");
+                if(fUser!=null) {
+                    userID = fUser.getUid();
+                }
+                FirebaseUser mFirebaseUser =mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser!=null){
+                    RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.child(parentDb).child(UserType).child(userID).exists()){
+                                snapshot.child(parentDb).child(UserType).child(userID).getValue(User.class);
+                                //Toast.makeText(LogInPage.this, "Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                Animatoo.animateSlideLeft(StartupActivity.this);
+                                finish();
+                            }
+                            else if(snapshot.child(parentDb).child(UserType2).child(userID).exists()){
+                                snapshot.child(parentDb).child(UserType).child(userID).getValue(Shopkeeper.class);
+                                //Toast.makeText(LogInPage.this, "Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(),HomeShopkeeper.class));
+                                Animatoo.animateSlideLeft(StartupActivity.this);
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(StartupActivity.this, "Main Failed to login with ! Please check your info", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(StartupActivity.this, "Failed to login with ! Please check your info", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }else {
                     Intent intent = new Intent(StartupActivity.this, LogInPage.class);
                     startActivity(intent);
                     Animatoo.animateSlideLeft(StartupActivity.this);
                     finish();
-//                }
+                }
             }
         }, splashTime);
-
     }
     @Override
     public void onBackPressed(){
